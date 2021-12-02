13865c939bc91280c453d04baf37db90e8190644
Owen-l4 <b0925138932@gmail.com>
2021-12-02 20:25:35 +0800

    add dashboard website

--- Web/app.js
+++ Web/app.js
@@ -1,10 +1,21 @@
-var hostname = "192.168.50.166"; 
-var port = "1883";  
-
 var connect = false;
 var client = new Paho.MQTT.Client(hostname, Number(port),"");
 
+var options = {
+    invocationContext: { host: "192.168.50.166", port:1883, clientId: "" },
+    timeout: 5,
+    keepAliveInterval: 60,
+    cleanSession: true,
+    useSSL: true,
+    //reconnect: true,
+    onSuccess: onConnect,
+    onFailure: onFail,
+    mqttVersion: 4
+};
+
 client.connect({onSuccess:OncConnect})
+
+
 
 document.getElementById("door").onclick = function(){
     message = new Paho.MQTT.Message(1);
@@ -19,5 +30,10 @@ function onConnect(context) {
     var connectionString = context.invocationContext.host + ":" + context.invocationContext.port + context.invocationContext.path;
     
     connected = true;
+
+}
+
+function onFail(context) {
+    connected = false;
 
 }

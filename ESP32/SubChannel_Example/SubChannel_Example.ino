7f00531916f7e0b57fc4b5b486102cd35d7aa62b
ed0419 <genden80@gmail.com>
2021-12-06 14:19:03 +0800

    add detect

--- ESP32/SubChannel_Example/SubChannel_Example.ino
+++ ESP32/SubChannel_Example/SubChannel_Example.ino
@@ -1,6 +1,11 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
 // WIFI資訊
 const char* ssid     = "TCIVS_CSE_IoT";
 const char* password = "MyPassW0rd";
@@ -13,6 +18,18 @@ const char ctrlTopic[] = "tcivs/ctrl/door";
 // 設定繼電器接腳
 const unsigned long relayPin = 25;
 const unsigned long SensorPin = 26;
+
+
+#define SCREEN_WIDTH 128 // 設定OLED螢幕的寬度像素
+#define SCREEN_HEIGHT 64 // 設定OLED螢幕的寬度像素
+#define OLED_RESET     -1 // Reset pin 如果OLED上沒有RESET腳位，將它設置為-1
+
+//OLED顯示器使用I2C連線並宣告名為display物件
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+bool OLEDStatus = true;
+
+
+
 WiFiClient espClient;
 PubSubClient client(espClient);
 unsigned long lastMsg = 0;
@@ -42,8 +59,26 @@ void setup_wifi() {
   Serial.println("WiFi 已連上^^");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+
+  Wire.begin(21,22);    //設定ESP32 I2C腳位
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3c)) { // 設定位址為0x3c
+    Serial.println(F("SSD1306 allocation failed")); //F(字串)：將字串儲存在flash並非在RAM
+    OLEDStatus = false;     //開啟OLED失敗
+  }
+  display.clearDisplay();       //清除螢幕
 }
 
+
+void OLED_Display() {
+  display.clearDisplay();       //清除螢幕
+  display.setCursor(0, 0);     // 設定起始點位置(0,16)
+  display.setTextSize(2);     //設定文字尺寸為2
+  display.setTextColor(WHITE);    //設定文字顏色為白色(亮點)
+  display.println("test"); //將HH存入RAM
+  display.display();      //顯示所設定文字
+  //delay(2000);    //延遲2秒
+}
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("收到來自 [");
   Serial.print(topic);
@@ -83,13 +118,15 @@ void reconnect() {
 
 
 // detector
-//void detect(){
-//  if (digitalRead(SensorPin)){
-//    digitalWrite(relayPin, HIGH);
-//    delay(1200);
-//    digitalWrite(relayPin, LOW);
-//  }
-//}
+void detect(){
+  Serial.println(digitalRead(SensorPin));
+  if (digitalRead(SensorPin)){
+    
+    digitalWrite(relayPin, HIGH);
+    delay(1200);
+    digitalWrite(relayPin, LOW);
+  }
+}
 
 void setup() {
   pinMode(relayPin, OUTPUT);
@@ -119,5 +156,6 @@ void loop() {
 //    client.publish(topic, msg);
   }
 
-//  detect();
+  detect();
+  OLED_Display();
 }
